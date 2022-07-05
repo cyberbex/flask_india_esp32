fc47354204d6cbec4be9a64f32ad34f72dd868f6
cyberbex <cyberbex@gmail.com>
2022-07-05 01:07:21 -0300

    blz

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 sensores = {
     "temperatura": 18,
@@ -10,7 +12,16 @@ sensores = {
 
 @app.route('/', methods=["GET"])
 def inicial():
-    return render_template("index.html", temperatura=sensores['temperatura'], amonia=sensores['amonia'])
+    str = 'eai bex'
+    return render_template("index.html")
+    # return render_template("index.html", temperatura=sensores['temperatura'], amonia=sensores['amonia'])
+    # return render_template("index.html", str=str)
+
+
+@app.route('/valores', methods=["GET"])
+def outra():
+    sensores['temperatura'] = sensores['temperatura'] + 1
+    return jsonify(sensores)
 
 
 @app.route('/sensores', methods=["POST"])
